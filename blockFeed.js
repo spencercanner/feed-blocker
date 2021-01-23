3310171d19a75ada2f384d86074281467415e59c
Spencer Canner <spencer.canner@gmail.com>
2021-01-23 10:12:01 -0500

    first commit

---
+++ blockFeed.js
@@ -0,0 +1,5 @@
+const mainElement = document.querySelector(".core-rail");
+
+const feed = mainElement.children[2];
+
+feed.style.display = "none";
---
+++ popup.js
@@ -0,0 +1,24 @@
+// Set up event handlers and inject send_links.js into all frames in the active
+// tab.
+window.onload = function () {
+  console.log("here");
+  document.getElementById("removeFeed").addEventListener("click", () => {
+    console.log("clicked");
+  });
+};
+
+chrome.runtime.onMessage.addListener(function (message, callback) {
+  if (message == "changeColor") {
+    chrome.tabs.executeScript({
+      code: 'document.body.style.backgroundColor="orange"',
+    });
+  }
+});
+
+//   "browser_action": { "default_popup": "popup.html" },
+//   "content_scripts": [
+//     {
+//       "matches": ["*://linkedin.com/*"],
+//       "js": ["/feeds.js"]
+//     }
+//   ],
