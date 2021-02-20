a2e45a30e353b28d8ad83f6af6b944d93c407771
Spencer Canner <spencer.canner@gmail.com>
2021-02-20 10:45:00 -0500

    restrict to linkedin and fix selector

--- blockFeed.js
+++ blockFeed.js
@@ -1,5 +1,14 @@
-const mainElement = document.querySelector(".core-rail");
+function toggleFeed() {
+  if (window.location.host.includes("linkedin")) {
+    const mainElement = document.querySelector("main");
+    const feed = mainElement.children[2];
 
-const feed = mainElement.children[2];
+    if (feed.style.display === "none") {
+      feed.style.display = "";
+    } else {
+      feed.style.display = "none";
+    }
+  }
+}
 
-feed.style.display = "none";
+toggleFeed();
--- feeds.js
+++ feeds.js
@@ -1,6 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   // No tabs or host permissions needed!
-  console.log("Turning " + tab.url + " red!");
   chrome.tabs.executeScript({
     file: "blockFeed.js",
   });
